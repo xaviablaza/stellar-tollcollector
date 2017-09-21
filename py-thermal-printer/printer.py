a8a6e826912f42cf48d101553ce321414ad25a86
Xavi Ablaza <xlablaza@gmail.com>
2017-09-21 13:02:31 -0700

    Added test code for qrcode generation

--- py-thermal-printer/printer.py
+++ py-thermal-printer/printer.py
@@ -5,6 +5,10 @@ from serial import Serial
 from struct import unpack
 from time import sleep
 
+# Stellar Paper Wallet Generation
+from stellar_base.keypair import Keypair
+import qrcode
+
 #===========================================================#
 # RASPBERRY PI (tested with Raspbian Jan 2012):
 # - Ensure that ttyAMA0 is not used for serial console access:
@@ -397,33 +401,33 @@ if __name__ == '__main__':
         sys.exit("ERROR: Serial port not found at: %s" % serialport)
 
     print "Testing printer on port %s" % serialport
-    p = ThermalPrinter(serialport=serialport)
-    p.print_text("\nHello maailma. How's it going?\n")
-    p.print_text("Part of this ")
-    p.bold()
-    p.print_text("line is bold\n")
-    p.bold(False)
-    p.print_text("Part of this ")
-    p.font_b()
-    p.print_text("line is fontB\n")
-    p.font_b(False)
-    p.justify("R")
-    p.print_text("right justified\n")
-    p.justify("C")
-    p.print_text("centered\n")
-    p.justify() # justify("L") works too
-    p.print_text("left justified\n")
-    p.upsidedown()
-    p.print_text("upside down\n")
-    p.upsidedown(False)
+#    p = ThermalPrinter(serialport=serialport)
+#    p.print_text("\nHello maailma. How's it going?\n")
+#    p.print_text("Part of this ")
+#    p.bold()
+#    p.print_text("line is bold\n")
+#    p.bold(False)
+#    p.print_text("Part of this ")
+#    p.font_b()
+#    p.print_text("line is fontB\n")
+#    p.font_b(False)
+#    p.justify("R")
+#    p.print_text("right justified\n")
+#    p.justify("C")
+#    p.print_text("centered\n")
+#    p.justify() # justify("L") works too
+#    p.print_text("left justified\n")
+#    p.upsidedown()
+#    p.print_text("upside down\n")
+#    p.upsidedown(False)
 
-    markup = """bl bold left
-ur underline right
-fc font b centred (next line blank)
-nl
-il inverse left
-"""
-    p.print_markup(markup)
+#    markup = """bl bold left
+#ur underline right
+#fc font b centred (next line blank)
+#nl
+#il inverse left
+#"""
+#    p.print_markup(markup)
 
     # runtime dependency on Python Imaging Library
     from PIL import Image
@@ -431,8 +435,15 @@ il inverse left
     data = list(i.getdata())
     w, h = i.size
     p.print_bitmap(data, w, h, True)
-    p.linefeed()
-    p.justify("C")
-    p.barcode_chr("2")
-    p.barcode("014633098808")
-    p.linefeed(3)
+
+    img = qrcode.make('SOME DATA HERE')
+    datatest = list(img.getdata())
+    w1, h1, = img.size
+    p.print_bitmap(datatest, w1, h1, True)
+
+
+#    p.linefeed()
+#    p.justify("C")
+#    p.barcode_chr("2")
+#    p.barcode("014633098808")
+#    p.linefeed(3)
